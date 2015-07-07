0c2426b9035a48302e7a891d15208cc2d18e578c
Arshdeep Singh <arshdeeptakkar@gmail.com>
2015-07-07 13:06:12 +0530

    added new classes

--- RandomWarriors/app/src/main/java/geekybytes/randomwarriors/CharacterMatrix.java
+++ RandomWarriors/app/src/main/java/geekybytes/randomwarriors/CharacterMatrix.java
@@ -1,7 +1,42 @@
 package geekybytes.randomwarriors;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 /**
  * Created by Arshdeep on 05-07-2015.
  */
 public class CharacterMatrix {
+
+
+
+    public HashMap charList = new HashMap ();
+    charList.SimpleEntry = ("Knight",0);
+    charList.put("Archer",1);
+    charList.put("Ninja",2);
+    charList.put("Spearman",3);
+    charList.put("Wizard",4);
+    charList.put("Berserker",5);
+    charList.put("Psycho",6);
+    charList.put("Barbarian",7);
+    charList.put("Robot",8);
+
+    Hashtable characterList = new Hashtable();
+
+    double charTable[][] = {
+        { 0.50, 0.80, 0.60, 0.20, 0.40, 0.40, 0.58, 0.40, 0.60 },
+        { 0.20, 0.50, 0.30, 0.80, 0.40, 0.60, 0.58, 0.20, 0.20 },
+        { 0.40, 0.70, 0.50, 0.60, 0.70, 0.80, 0.58, 0.60, 0.60 },
+        { 0.80, 0.20, 0.20, 0.50, 0.30, 0.20, 0.58, 0.20, 0.40 },
+        { 0.60, 0.60, 0.30, 0.70, 0.50, 0.80, 0.58, 0.80, 0.20 },
+        { 0.60, 0.40, 0.20, 0.80, 0.20, 0.50, 0.58, 0.80, 0.80 },
+        { 0.42, 0.42, 0.42, 0.42, 0.42, 0.42, 0.50, 0.42, 0.42 },
+        { 0.60, 0.80, 0.40, 0.80, 0.20, 0.20, 0.58, 0.50, 0.60 },
+        { 0.40, 0.60, 0.40, 0.60, 0.80, 0.20, 0.58, 0.20, 0.50 }
+    };
+
+    public double getWinningProbability(int firstCharIndex,int secondCharIndex)
+    {
+        return charTable[firstCharIndex][secondCharIndex];
+    }
 }
--- RandomWarriors/app/src/main/java/geekybytes/randomwarriors/OfflineCharacterFight.java
+++ RandomWarriors/app/src/main/java/geekybytes/randomwarriors/OfflineCharacterFight.java
@@ -1,7 +1,34 @@
 package geekybytes.randomwarriors;
 
+import java.util.Random;
+
 /**
  * Created by Arshdeep on 05-07-2015.
  */
 public class OfflineCharacterFight {
+
+    private String selectedChar = "";
+
+
+    protected void  OfflineCharacterFight(String character )
+    {
+        this.selectedChar = character;
+    }
+
+    protected boolean fightSimulation()
+    {
+        Random randomGenerator = new Random();
+        boolean win = false;
+
+        int Randnum = randomGenerator.nextInt(101);
+        int secondChar = randomGenerator.nextInt(10);
+
+        CharacterMatrix charMatrix = new CharacterMatrix();
+        Integer firstCharNumb = (Integer) charMatrix.characterList.get(selectedChar);
+        double winProb = charMatrix.getWinningProbability(1,secondChar)*100;
+        if (Randnum>winProb)
+            win = true;
+
+        return win;
+    }
 }
